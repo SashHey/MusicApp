738426cac4382e0dd4c3f653ee14a38a5fb2a43e
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-04-20 15:23:26 +0200

    Added array for songs and for loop to display them

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -15,11 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var songShowTxt = findViewById<TextView>(R.id.songDisplayId)
+        var songDisplay = ""
 
         //accessing the Song class to populate song objects with specific attributes
         var song1 = Song("Choosin' Texas", "Ella Langley", 2026, "3.5")
         var song2 = Song("Man I Need", "Olivia Dean", 2026, "2.5")
         var song3 = Song("I Just Might", "Bruno Mars")
+
+        //creating an array to hold the song objects
+        var songs = arrayOf<Song>(song1, song2, song3)
+
+        //for loop to loop through and display songs from the songs array
+        for (song in songs){
+            songDisplay += "${song.DisplaySong()}\n\n"
+        }
+
+        songShowTxt.text = songDisplay
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
